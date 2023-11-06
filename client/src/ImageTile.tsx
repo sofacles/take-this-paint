b6fe0e95332349467593b06641021c38a419deee
Steve Degermark <sofacles@yahoo.com>
2023-11-06 07:43:28 -0800

    Make imageless ImageTile take up a little more space, customize icon

--- client/src/ImageTile.tsx
+++ client/src/ImageTile.tsx
@@ -11,16 +11,21 @@ export const ImageTile = ({ paintUnit }: PaintTileProps) => {
     <Link
       to="/SendMail"
       state={paintUnit}
-      className={`m-2 border border-black-4 ${
-        paintUnit.imageName ? "" : "w-[200px] h-[200px]"
-      }`}
+      className={`m-2 border j border-black-4 `}
     >
-      <div className={`p-4 min-h-full`} style={rgbStyle}>
-        {paintUnit.imageName && (
+      <div className={`p-4 h-128 min-h-full`} style={rgbStyle}>
+        {paintUnit.imageName ? (
           <img
             alt="paint color"
             width={200}
             src={`uploads/resized/${paintUnit.imageName}`}
+          />
+        ) : (
+          <img
+            alt="paint color"
+            className="opacity-0"
+            width={200}
+            src={`uploads/blank.png`}
           />
         )}
         <div className="p-0 m-0 w-full mb-4 ">{name}</div>
