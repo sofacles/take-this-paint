374db1f918b942f74f737d866462170e753cfb36
Steve Degermark <sofacles@yahoo.com>
2023-11-06 19:01:47 -0800

    Delete and gitignore shellscrips

--- shellscripts/deployOct25Aws.sh
+++
@@ -1,8 +0,0 @@
-docker run --name mongodb -d -p 27017:27017 -e MONGO_INITDB_ROOT_USERNAME=timmy -e MONGO_INITDB_ROOT_PASSWORD=spider mongodb/mongodb-community-server:6.0-ubi8
-
-mongoimport --db meals --collection meals2023 --file route/to/archive.json --jsonArray -u timmy -p spider --authenticationDatabase=admin
-
-
-run npm run build in the server folder
-I added `"start:prod": "node build/server/index.js"` to server/package.json to test it locally
-run npm run build in the client folder
