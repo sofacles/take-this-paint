ab80db575b0bf67a2441474c71e2e14dd73e4092
Steve Degermark <sofacles@yahoo.com>
2023-12-17 08:40:27 -0800

    zipcode is always in edit mode

--- client/src/view-paint/ZipCode.tsx
+++ client/src/view-paint/ZipCode.tsx
@@ -25,63 +25,58 @@ const ZipCode = (props: ZipCodePropsType) => {
     props.onUpdated(milesFrom, zipCode);
   };
 
-  if (editMode) {
-    return (
-      <FormProvider {...methods}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="flex flex-row">
-            <span>Not new</span>
-            <label htmlFor="milesFrom"></label>
-            <input
-              type="text"
-              {...register("milesFrom", {
-                required: "enter a number of miles",
-                value: milesFrom,
-              })}
-            />
-            from
-            <label htmlFor="milesFrom"></label>
-            <input
-              type="text"
-              {...register("zipCode", {
-                required: "enter zip code",
-                validate: (r) => /^\d{5}$/.test(r) || "enter 5 digits",
-                value: zipCode,
-              })}
-            />
-            <input type="submit" />
-          </div>
-          <p className="my-0 pt-0 text-red-400 ml-10  text-sm">
-            <ErrorMessage
-              errors={errors}
-              name="milesFrom"
-              render={({ message }) => <span className="mr-16">{message}</span>}
-            />
-            <ErrorMessage
-              errors={errors}
-              name="zipCode"
-              render={({ message }) => <span>{message}</span>}
-            />
-          </p>
-        </form>
-      </FormProvider>
-    );
-  } else {
-    return (
-      <div>
-        <div className="p-2" role="button" onClick={() => setEditMode(true)}>
-          <div className="">
-            <span>
-              Not new paint {milesFrom} miles from {zipCode}
-            </span>
-            <span className="inline-flex ml-3">
-              <EditPen />
-            </span>
-          </div>
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h2 className="visible sm:invisible">
+          Show me opened paints that people want to get rid of within
+        </h2>
+        <div className="flex max-h-[3rem]">
+          <label htmlFor="milesFrom" className="mr-2 invisible sm:visible ">
+            Show me opened paints that people want to get rid of within
+          </label>
+          <input
+            className="mr-2 w-[50px]"
+            type="text"
+            {...register("milesFrom", {
+              required: "enter a number of miles",
+              value: milesFrom,
+            })}
+          />
+
+          <label htmlFor="milesFrom" className="mr-2">
+            miles of
+          </label>
+          <input
+            type="text"
+            className="mr-2 w-[150px]"
+            {...register("zipCode", {
+              required: "enter zip code",
+              validate: (r) => /^\d{5}$/.test(r) || "enter 5 digits",
+              value: zipCode,
+            })}
+          />
+          <input
+            className="bg-emerald-300 border-2 hover:bg-emerald-100 border-emerald-800 p-2 rounded-md"
+            type="submit"
+            value="OK"
+          />
         </div>
-      </div>
-    );
-  }
+        <p className="my-0 pt-0 text-red-400 ml-10  text-sm">
+          <ErrorMessage
+            errors={errors}
+            name="milesFrom"
+            render={({ message }) => <span className="mr-16">{message}</span>}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="zipCode"
+            render={({ message }) => <span>{message}</span>}
+          />
+        </p>
+      </form>
+    </FormProvider>
+  );
 };
 
 export default ZipCode;
