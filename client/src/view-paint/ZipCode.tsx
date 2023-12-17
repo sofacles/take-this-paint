cb711a3ed0f563fda0e9b441a48581318402b0cb
Steve Degermark <sofacles@yahoo.com>
2023-12-17 09:33:17 -0800

    make zip code form a little prettier

--- client/src/view-paint/ZipCode.tsx
+++ client/src/view-paint/ZipCode.tsx
@@ -28,15 +28,18 @@ const ZipCode = (props: ZipCodePropsType) => {
   return (
     <FormProvider {...methods}>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <h2 className="visible sm:invisible">
-          Show me opened paints that people want to get rid of within
+        <h2 className="block md:hidden">
+          Show me opened paints that people want to get rid of
         </h2>
-        <div className="flex max-h-[3rem]">
-          <label htmlFor="milesFrom" className="mr-2 invisible sm:visible ">
-            Show me opened paints that people want to get rid of within
+        <div className="items-baseline flex max-h-[3rem]">
+          <span className="hidden md:inline mr-2 md:mr-1">
+            Show me opened paints that people want to get rid of
+          </span>
+          <label htmlFor="milesFrom" className="mr-2  ">
+            within
           </label>
           <input
-            className="mr-2 w-[50px]"
+            className="mr-2 w-[50px] rounded-md h-6"
             type="text"
             {...register("milesFrom", {
               required: "enter a number of miles",
@@ -49,7 +52,7 @@ const ZipCode = (props: ZipCodePropsType) => {
           </label>
           <input
             type="text"
-            className="mr-2 w-[150px]"
+            className="mr-2 w-[80px]  rounded-md h-6"
             {...register("zipCode", {
               required: "enter zip code",
               validate: (r) => /^\d{5}$/.test(r) || "enter 5 digits",
@@ -57,7 +60,7 @@ const ZipCode = (props: ZipCodePropsType) => {
             })}
           />
           <input
-            className="bg-emerald-300 border-2 hover:bg-emerald-100 border-emerald-800 p-2 rounded-md"
+            className="bg-emerald-100 flex py-0 h-6 border-spacing-1 hover:bg-emerald-300 hover:shadow-md border-emerald-800 p-2 rounded-md"
             type="submit"
             value="OK"
           />
